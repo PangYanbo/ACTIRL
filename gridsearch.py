3306aebd230210cddc20cee5cf984ff16f557c0a
PangYanbo <pybdtc@gmail.com>
2020-04-09 10:18:48 +0000

    aws 2nd

--- gridsearch.py
+++ gridsearch.py
@@ -3,11 +3,7 @@ import train
 
 
 def main():
-<<<<<<< HEAD
     param_set = [(q, i) for q in np.arange(0.01, 0.11, 0.01) for i in np.arange(0.01, 0.11, 0.01)]
-=======
-    param_set = [(q, i) for q in np.arange(0.01, 0.11, 0.01) for i in np.arange(0.02, 0.11, 0.02)]
->>>>>>> 34eac65aa5c0a2b23c90c27fdc76b308af8af34d
     for param in param_set:
         print(param)
         train.train(param[0], param[1])
