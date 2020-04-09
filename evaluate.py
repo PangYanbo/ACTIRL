34eac65aa5c0a2b23c90c27fdc76b308af8af34d
PangYanbo <pybdtc@gmail.com>
2020-04-09 18:53:30 +0900

    first commit

---
+++ evaluate.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('ggplot')
+
+
+def main():
+    demo = []
+    sim = []
+
+    state_idx = {'0': 'home', '1': 'work', '2': 'school', '3': 'other', '4': 'train', '5': 'vehicle', '6': 'walk'}
+
+    df_truth = pd.DataFrame({'0': np.zeros(6),
+                             '1': np.zeros(6),
+                             '2': np.zeros(6),
+                             '3': np.zeros(6),
+                             '4': np.zeros(6),
+                             '5': np.zeros(6),
+                             '6': np.zeros(6)
+    })
+
+    df_sim = pd.DataFrame({'0': np.zeros(6),
+                       '1': np.zeros(6),
+                       '2': np.zeros(6),
+                       '3': np.zeros(6),
+                       '4': np.zeros(6),
+                       '5': np.zeros(6),
+                       '6': np.zeros(6)
+    })
+
+    with open('test.csv', 'r') as f:
+        for line in f:
+            line = line.strip('\n')
+            demo.append(line)
+
+    with open('out.csv', 'r') as f:
+        for line in f:
+            line = line.strip('\n')
+            sim.append(line)
+
+    for t in range(6):
+        for i in range(len(demo)):
+            df_truth[demo[i][t]][t] += 1
+            df_sim[sim[i][t]][t] += 1
+
+    plt.figure(figsize=(12, 8))
+    plt.subplots_adjust(wspace=0.3, hspace=0.5)
+    plt.title('Results of activity ratio at each time step')
+    for i in range(7):
+        plt.subplot(3, 3, i+1)
+        plt.plot(range(6), df_truth[str(i)]/len(demo), label='truth')
+        plt.plot(range(6), df_sim[str(i)]/len(demo), label='sim')
+        plt.ylim(0, 1)
+        plt.legend()
+        plt.title(state_idx[str(i)])
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
